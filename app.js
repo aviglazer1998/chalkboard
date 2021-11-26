ace3637dfec322002451b6513157da6ddd5e20a9
AviGlazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-25 21:29:16 -0500

    added for heroku

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.use(bodyParser.json());
+// Express modules / packages
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// Express modules / packages
+
+app.use(express.static("public"));
+// load the files that are in the public directory
+
+app.get("/", (request, response) => {
+	response.sendFile("/home/runner/chalkboard/index.html");
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+	console.log(`App listening on port ${PORT}`);
 });
