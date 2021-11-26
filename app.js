6ac59cccdadf8ef24c1718fe4e3b223dc6f56235
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-25 21:49:34 -0500

    added mongodb

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const routes = require("./routes/api");
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/<reactreadinglist>", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+	console.log("Mongoose is connected");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Express modules / packages
 
