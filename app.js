eb658e52d8b44144e9a8440738dbdc481cb3ee11
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-25 22:06:43 -0500

    reset for heroku

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const express = require("express");
-const path = require("path");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/<reactreadinglist>", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-	console.log("Mongoose is connected");
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Express modules / packages
 
 app.use(bodyParser.urlencoded({ extended: true }));
+// Express modules / packages
 
 app.use(express.static("public"));
+// load the files that are in the public directory
 
 app.get("/", (request, response) => {
-	response.sendFile("/index.html");
+	response.sendFile("/home/runner/chalkboard/index.html");
 });
 
 app.listen(PORT, () => {
