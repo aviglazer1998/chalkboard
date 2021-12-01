f58c15148ed9df52561de33039bd8d3921f34ded
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-11-30 19:04:57 -0500

    added post route

--- models/Students.js
+++ models/Students.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-	name: String,
+	firstName: String,
+	lastName: String,
 	email: String,
 	password: String,
 	classes: [
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
-const Student = require('./models/Students');
+const router = express.Router();
+const mongoose = require("mongoose");
+const Student = require("./models/Students");
 
-
-const dbURI = 'mongodb+srv://aviglazer:Password123@chalkboard.mc7fa.mongodb.net/chalkboard?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result)=> app.listen(8000))
-.catch((err)=> console.log(err));
+const dbURI = "mongodb+srv://aviglazer:Password123@chalkboard.mc7fa.mongodb.net/chalkboard?retryWrites=true&w=majority";
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => app.listen(8000))
+	.catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 
@@ -17,34 +18,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-	response.sendFile("/home/runner/chalkboard/index.html");
+	response.sendFile("index.html");
 });
 
-
-app.get('/sign-up',(req,res)=>{
+app.post("/sign-up", (req, res) => {
 	const student = new Student({
-		name: 'miriam A',
-		email: 'miriamA@gmail.com',
-		password: 'Password123!',
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password,
+		classes: [],
+	});
+
+	student.save();
+});
+
+app.get("/sign-up", (req, res) => {
+	const student = new Student({
+		firstname: "miriam",
+		lastname: "A",
+		email: "miriamA@gmail.com",
+		password: "Password123!",
 		classes: [
 			{
-				className: 'CSCI 355',
-				classId: '355',
-				classStart: '8:00',
-				classEnd: '9:00',
-				classDays: 'MWF',
-				classInstructor: 'Abromov',
-				classDescription: 'This is a class',
+				className: "CSCI 355",
+				classId: "355",
+				classStart: "8:00",
+				classEnd: "9:00",
+				classDays: "MWF",
+				classInstructor: "Abromov",
+				classDescription: "This is a class",
 			},
 		],
 	});
-	student.save()
-	.then((result)=>{
-		res.send(result);
-	})
-	.catch((err)=>{
-		console.log(err);
-	});
+	student
+		.save()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-
+app.listen(() => {
+	console.log(`App listening on port 8000`);
+});
