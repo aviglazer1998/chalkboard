beeab8fbaff029c7d31f587a9dd3c6c981c2049a
malyesh <74512928+malyesh@users.noreply.github.com>
2021-12-02 23:18:17 -0500

    only one checkbox allowed at a time

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,6 +1,11 @@
-function onlyOne(checkbox){
-    var checkboxes = document.getElementsByClassName('boxes')
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    })
+function onlyOne(id){
+    // var checkboxes = document.getElementsByClassName('boxes')
+    // checkboxes.forEach((item) => {
+    //     if (item !== checkbox) item.checked = false
+    // })
+    for (var i = 1;i <= 2; i++)
+    {
+        document.getElementById(i).checked = false;
+    }
+    document.getElementById(id).checked = true;
 }
