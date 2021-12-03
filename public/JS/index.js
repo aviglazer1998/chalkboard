d0b09a8fc266a7c12b74430e289c5de6adbd2591
malyesh <74512928+malyesh@users.noreply.github.com>
2021-12-02 23:22:00 -0500

    fixed checkboxes

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,8 +1,5 @@
 function onlyOne(id){
-    // var checkboxes = document.getElementsByClassName('boxes')
-    // checkboxes.forEach((item) => {
-    //     if (item !== checkbox) item.checked = false
-    // })
+    
     for (var i = 1;i <= 2; i++)
     {
         document.getElementById(i).checked = false;
