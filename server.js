f1a42edfcb5abeba491b7f059dc67b16e9704067
malyesh <74512928+malyesh@users.noreply.github.com>
2021-12-20 16:37:31 -0500

    got waitlist to work!

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get('/admin-view', redirectLogin, (req, res) => {
 
 app.get('/:studentId/studentHomePage', redirectLogin, (req, res) => {
   const { studentId } = req.params;
-  Student.findOne({ where: { id: studentId } }, (err, student) => {
+  Student.findById(studentId, (err, student) => {
     // const classes = student.classes;
     // console.log(classes);
     Class.find({ classStudents: student }, function (err, classData) {
@@ -176,32 +176,32 @@ app.get('/:studentId/studentHomePage', redirectLogin, (req, res) => {
   });
 });
 
-app.put('/:studentId/studentHomePage/:classId', redirectLogin, (req, res) => {
+//add student to waitlist
+app.get('/:studentId/studentHomePage/:classId', redirectLogin, (req, res) => {
   const { studentId } = req.params;
   const { classId } = req.params;
-  Class.findOne({ where: { id: classId } }, (err, course) => {
-    Student.findOne({ where: { id: studentId } }, (err, student) => {
-      course.students.add(student);
-
-      course.save(function (err) {
-        if (err) {
-          console.log('couldnt add course');
-          render(':studentId/studentHomePage/:classId');
-        } else {
-          console.log(course);
-          res.render(':studentId/studentHomePage/:classId', {
-            user: student,
-            course: course,
-          });
-        }
+  //   console.log('studentId: ' + studentId);
+  console.log('classId: ' + classId);
+  Class.findById(classId, (err, course) => {
+    Student.findById(studentId, (err, student) => {
+      course.classWaitlist.push(student);
+      course.save();
+      console.log(course.classWaitlist);
+      res.render('studentHomePage', {
+        // student: student,
+        // course: course,
+        practices: null,
+        user: student,
       });
     });
+    console.log(course.id);
   });
 });
 
 app.get('/:instructorId/instructorHomePage', redirectLogin, (req, res) => {
   const { instructorId } = req.params;
-  Instructor.findOne({ where: { id: instructorId } }, (err, instructor) => {
+  Instructor.findById(instructorId, (err, instructor) => {
+    console.log(instructor);
     Classes.find({}, (err, classes) => {
       res.render('instructorHomePage', {
         classes: classes,
@@ -227,72 +227,68 @@ app.get('/:id/class-search', redirectLogin, (req, res) => {
   res.render('searchClasses');
 });
 
-app.get('/:id/search-result?', redirectLogin, (req, res) => {
+app.get('/:id/searchClasses', redirectLogin, (req, res) => {
+  res.render('searchClasses');
+});
+
+app.post('/:id/search-result?', redirectLogin, (req, res) => {
   const { id } = req.params;
-  Class.findOne(
-    {
-      where: {
-        className: req.body.className,
-      },
-    },
-    (err, course) => {
-      if (course) {
-        const array = course.classInstructors;
-        const newArray = [];
-        array.forEach(function (instructor) {
-          Instructor.findOne({ where: { id: instructor.id } }, (err, i) => {
-            // console.log(i);
-            newArray.push(i);
-            // console.log(newArray);
-          });
+  Class.findOne({ className: req.body.className }, (err, course) => {
+    if (course) {
+      const array = course.classInstructors;
+      const newArray = [];
+      array.forEach(function (instructor) {
+        Instructor.findOne({ where: { id: instructor.id } }, (err, i) => {
+          // console.log(i);
+          newArray.push(i);
+          // console.log(newArray);
         });
-        Instructor.find({}, (err, instructors) => {
-          res.render('searchResults', {
-            course: course,
-            id: id,
-            instructors: instructors,
-          });
+      });
+      Instructor.find({}, (err, instructors) => {
+        res.render('searchResults', {
+          course: course,
+          id: id,
+          instructors: instructors,
         });
-      } else {
-        res.redirect('searchClasses');
-      }
+      });
+    } else {
+      res.redirect('searchClasses');
     }
-  );
+  });
 });
 
 app.get('/:id/class-search-student', redirectLogin, (req, res) => {
   res.render('studentSearchClasses');
 });
 
-app.get('/:id/search-result-student?', redirectLogin, (req, res) => {
-  console.log('in search result');
+app.get('/:id/studentSearchClasses', redirectLogin, (req, res) => {
+  res.render('studentSearchClasses');
+});
+
+app.post('/:id/search-result-student?', redirectLogin, (req, res) => {
   const { id } = req.params;
-  Class.findOne(
-    {
-      where: {
-        className: req.body.className,
-        // classId: req.body.classId,
-        // ClassInstructor: req.body.instructorName,
-      },
-    },
-    (err, course) => {
-      if (course) {
-        const instructorId = course.classInstructors[0];
-        Instructor.findOne(
-          { where: { id: instructorId } },
-          (err, instructor) => {
-            res.render('studentSearchResult', {
-              course: course,
-              user: instructor,
-              id: id,
-            });
-          }
-        );
-      } else {
-        res.redirect('studentSearchClasses');
-      }
+  Class.findOne({ className: req.body.className }, (err, course) => {
+    if (course) {
+      const array = course.classInstructors;
+      const newArray = [];
+      array.forEach(function (instructor) {
+        Instructor.findOne({ where: { id: instructor.id } }, (err, i) => {
+          // console.log(i);
+          newArray.push(i);
+          // console.log(newArray);
+        });
+      });
+      Instructor.find({}, (err, instructors) => {
+        res.render('studentSearchResult', {
+          course: course,
+          id: id,
+          instructors: instructors,
+        });
+      });
+    } else {
+      res.redirect('studentSearchClasses');
     }
-  );
+  });
 });
 
 app.get('/all-instructors', redirectLogin, (req, res) => {
@@ -348,7 +344,7 @@ app.get('/:id/instructorCoursePage/:courseId', redirectLogin, (req, res) => {
   const { courseId } = req.params;
   console.log(courseId);
 
-  Class.findOne({ where: { classId: courseId } }, (err, course) => {
+  Class.findById(courseId, (err, course) => {
     Instructor.find({}, (err, instructors) => {
       res.render('instructorCoursePage', {
         course: course,
@@ -362,7 +358,7 @@ app.get('/:id/instructorCoursePage/:courseId', redirectLogin, (req, res) => {
 app.get('/:id/studentCoursePage/:courseId', redirectLogin, (req, res) => {
   const { courseId } = req.params;
   console.log(courseId);
-  Class.findOne({ where: { classId: courseId } }, (err, course) => {
+  Class.findById(courseId, (err, course) => {
     console.log(course);
     res.render('studentCoursePage', {
       course: course,
@@ -389,7 +385,7 @@ app.get('/classResults', redirectLogin, (req, res) => {
 
 app.get('/:id/createCourse', redirectLogin, (req, res) => {
   const { id } = req.params;
-  Instructor.findOne({ where: { id: id } }, (err, instructor) => {
+  Instructor.findById(id, (err, instructor) => {
     console.log(instructor);
     res.render('createCourse', {
       instructor: instructor,
@@ -402,7 +398,7 @@ app.post('/:id/createCourse', redirectLogin, (req, res) => {
   console.log('in create course');
   const { id } = req.params;
   console.log(id);
-  Instructor.findOne({ where: { id: id } }, (err, instructor) => {
+  Instructor.findById(id, (err, instructor) => {
     const newClass = new Class({
       className: req.body.className,
       // classId: ,
@@ -421,9 +417,11 @@ app.post('/:id/createCourse', redirectLogin, (req, res) => {
     console.log(newClass);
     // console.log(instructor.courses);
     instructor.classes.push(newClass);
+    instructor.save();
+    console.log(instructor);
     res.render('instructorHomePage', {
       user: instructor,
-      practices: null,
+      classes: instructor.classes,
     });
   });
 });
@@ -444,7 +442,7 @@ app.get('/deleteCourse', redirectLogin, (req, res) => {
 
 app.get('/:id/roster', redirectLogin, (req, res) => {
   const { id } = req.params;
-  Instructor.findOne({ where: { id: id } }, (err, instructor) => {
+  Instructor.findById(id, (err, instructor) => {
     Class.find({}, (err, classes) => {
       res.render('roster', {
         user: instructor,
